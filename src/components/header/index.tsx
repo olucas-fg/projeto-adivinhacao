4764cc700b2872900bcf37e10d4b2f6911b78db1
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-30 17:56:55 -0300

    finalizando o header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/logo.svg"
+import restart from "../../assets/restart.svg"
 
 import styles from "./styles.module.css"
 
@@ -6,6 +7,16 @@ export function Header() {
   return (
     <div className={styles.container}>
       <img src={logo} alt="Logo" />
+
+      <header>
+        <span>
+          <strong>5</strong> de 10 tentativas
+        </span>
+
+        <button type="button">
+          <img src={restart} alt="ícone de reiniciar" />
+        </button>
+      </header>
     </div>
   )
 }

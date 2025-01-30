70e5347cda92ff145362e441e459415c86a2f3a6
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-30 19:33:16 -0300

    criando letters used

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
 import { Input } from "./components/Input"
 import { Button } from "./components/Button"
+import { LettersUsed } from "./components/LettersUsed"
 
 export default function App() {
 
@@ -33,6 +34,8 @@ export default function App() {
           <Input autoFocus maxLength={1} placeholder="?"></Input>
           <Button title="Confirmar"></Button>
         </div>
+
+        <LettersUsed></LettersUsed>
       </main>
     </div>
   )
---
+++ src/components/LettersUsed/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css"
+
+export function LettersUsed() {
+  return <div className={styles.lettersUsed}>
+    <h5>Letras utilizadas</h5>
+  </div>
+}

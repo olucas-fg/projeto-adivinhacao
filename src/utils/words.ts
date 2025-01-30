065179271d47aead23eedff9f5c6682d21729005
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-30 15:27:35 -0300

    primeiro commit

---
+++ src/utils/words.ts
@@ -0,0 +1,13 @@
+export type Challenge = {
+  id: number
+  word: string
+  tip: string
+}
+
+export const WORDS: Challenge[] = [
+  {id: 1, word: "CSS", tip: "Linguagem de estilos"},
+  {id: 2, word: "REACT", tip: "Biblioteca para criar interfaces Web"},
+  {id: 3, word: "HTML", tip: "Linguagem de marcação"},
+  {id: 4, word: "Javascript", tip: "Uma das linguagens de programação mais utilizadas no mundo"},
+  {id: 5, word: "Typescript", tip: "Pata adicionar tipagem no Javascript"},
+]

546e944ca66796c1fdd3e616ce4d702c19b88d2f
Lucas Figueiredo <olucasfg@outlook.com>
2025-02-01 07:23:58 -0300

    verificando acertos do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { Button } from "./components/Button"
 import { LettersUsed, LetterUsedProps } from "./components/LettersUsed"
 
 export default function App() {
+  const [score, setScore] = useState(0)
   const[attempt, setAttempts] = useState(0)
   const [letter, setLetter] = useState("")
   const [challenge, setChallenge] = useState<Challenge | null>(null)
@@ -30,6 +31,33 @@ export default function App() {
     setLetter("")
   }
 
+  function handleConfirm() {
+    if (!challenge) {
+      return
+    }
+
+    if(!letter.trim()){
+      return alert ("Digite uma letra")
+    }
+
+    const value = letter.toUpperCase()
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value)
+
+    if (exists) {
+      return alert ("Você ja utilizou a letra " + value)
+    }
+
+    const hits = challenge.word.toUpperCase().split("").filter((char) => char === value).length
+
+    const correct = hits > 0
+    const currentScore = score + hits 
+
+    setLettersUsed((prevState) => [...prevState, {value, correct}])
+    setScore(currentScore)
+
+    setLetter("")
+  }
+
   useEffect(() => {
     startGame()
   }, [])
@@ -43,7 +71,7 @@ export default function App() {
       <main>
         <Header current={attempt} max={10} onRestart={handleRestartGame} />
 
-        <Tip tip="Uma das linguagens de programação mais utilizada" />
+        <Tip tip={challenge.tip} />
 
         <div className={styles.word}>
           {
@@ -56,8 +84,8 @@ export default function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?"></Input>
-          <Button title="Confirmar"></Button>
+          <Input autoFocus maxLength={1} placeholder="?" value= {letter}  onChange={(e) => setLetter(e.target.value)}></Input>
+          <Button title="Confirmar" onClick={handleConfirm}></Button>
         </div>
 
         <LettersUsed data={lettersUsed}></LettersUsed>

1a19e29c4aad2da5380682854a5e977de9af914b
Lucas Figueiredo <olucasfg@outlook.com>
2025-02-01 08:01:05 -0300

    fim do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,7 @@ export default function App() {
     const exists = lettersUsed.find((used) => used.value.toUpperCase() === value)
 
     if (exists) {
+      setLetter("")
       return alert("Você ja utilizou a letra " + value)
     }
 
@@ -60,9 +61,32 @@ export default function App() {
     setLetter("")
   }
 
+  function endGame (message: string) {
+    alert(message)
+    startGame()
+  }
+
   useEffect(() => {
     startGame()
   }, [])
+
+  useEffect(()=> {
+    if(!challenge){
+      return
+    }
+
+    setTimeout(() => {
+      if (score === challenge.word.length) {
+        return endGame("Parabéns, você descobriu a palavra!")
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPTS_MARGIN
+
+      if(lettersUsed.length === attemptLimit) {
+        return endGame("Que pena, você usou todas as tentativas!")
+      }
+    }, 200)
+  }, [score, lettersUsed.length])
 
   if (!challenge) {
     return
